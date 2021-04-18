8617418e52af9cd15cff2ac7df15f64bdee6b53b
elh4m <77781922+elh4m@users.noreply.github.com>
2021-04-18 13:54:49 +0200

    Add files via upload

--- final_assignment/scripts/exam_final.py
+++ final_assignment/scripts/exam_final.py
@@ -3,10 +3,15 @@
 import rospy
 from std_srvs.srv import *
 import time
+#To suspend excution for certain amount of time
 from time import sleep
+#Common messages needed for velocities
 from geometry_msgs.msg import Twist
+#Common messages needed for navigation
 from move_base_msgs.msg import MoveBaseActionGoal
+#To intract with action server and action clint
 from actionlib_msgs.msg import GoalStatusArray
+#import for custom services
 from my_srv.srv import final_exercise
 
 target_reached_status = 0
@@ -17,42 +22,40 @@ def clbk_move_base_status(msg):
     global target_reached_status
     if (len(msg.status_list) > 0):
         if msg.status_list[0].status == 3:
-	        arget_reached_status = 1
+	        target_reached_status = 1
  
 
 def main():
-# initializing the node(exam_final) as the server
-    rospy.init_node('final_user_req')
+#Initializing the node(Final_code) as the sever 
+    rospy.init_node('Final_code')
     global target_reached_status, wall_follower_client
 
     random_index_service = rospy.ServiceProxy('/final_exercise', final_exercise)
-#node used to subscribe on topic /move_base/status with a message GoalStatusArray from actionlib_msgs
+#Node used to subscribe on topic /move_base/status with a message GoalStatusArray from actionlib_msgs
     move_base_status = rospy.Subscriber('/move_base/status', GoalStatusArray, clbk_move_base_status, queue_size = 1)
-#node used to publish on topic /move_base/goal with a message MoveBaseActionGoal from move_base_msgs
+#Node used to publish on topic /move_base/goal with a message MoveBaseActionGoal from move_base_msgs
     new_target_pub = rospy.Publisher('/move_base/goal', MoveBaseActionGoal, queue_size = 1)
-#service client used to communicate with the user interface
+#Service client used to communicate with the user interface      
     wall_follower_client = rospy.ServiceProxy('/wall_follower_switch', SetBool)
-#node used to publish on topic /cmd_vel with a message Twist from geometry_msg
+#Node used to publish on topic /cmd_vel with a message Twist from geometry_msgs
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 1)
-#coordinates for the desired possible targets
+#Coordinates for the desired possible targets    
     random_targets = [(-4,-3), (-4,2), (-4,7), (5,-7), (5,-3), (5,1)]
 
     print("\nInitializing...\n")
     
     rate = rospy.Rate(20)
-    
     while not rospy.is_shutdown():
-#to take input from the user to perform 4 different tasks
-
+#To take input from the user to perform 4 differnt task in the program
         print("""\nEnter integers from 1 to 4 to execute the following behaviors:
      (1) Move randomly in the environment, by choosing 1 out of 6 possible target positions.
      (2) Enter the next target position out of the possible six and reach it.
      (3) Start following the external walls.
-     (4) Stop in the last position.""")
-#taking input from the user
+     (4) To enter standby position.""")
+#Takes input from the user
         x = int(input("\nEnter a number from 1 to 4 corresponding to the chosen robot behavior: "))
 
- #when the user gives input as 1 the robot moves randonly by choosing 1 coordinate out of all possible given coordinates       
+#When the user gives input as 1 the robot moves randomly by choosing 1 coordinate out of all possible given coordinated         
         if (x == 1):
             resp = wall_follower_client (False)
             resp = random_index_service (1,6)
@@ -61,25 +64,25 @@ def main():
             print("\nNew Target: (" + str(random_targets[rand_index -1][0]) + ", " + str(random_targets[rand_index -1][1]) + ")")
 
             MoveBase_msg = MoveBaseActionGoal()
- #setting frame_id to the map
+#Setting frame_id to the map            
             MoveBase_msg.goal.target_pose.header.frame_id = "map"
-#set orientation W            
+#Set orientation W             
             MoveBase_msg.goal.target_pose.pose.orientation.w = 1
-#for using x as input coordinate at position x           
+#For using x as input coordinate at position x             
             MoveBase_msg.goal.target_pose.pose.position.x = random_targets[rand_index -1][0]
-#for using y as input coordinate at position y
+#For using y as input coordinate at position y 
             MoveBase_msg.goal.target_pose.pose.position.y = random_targets[rand_index -1][1]
-#publishing MoveBase_msg in type of MoveBaseActionGoal            
+#Publishing MoveBase_msg in type of MoveBaseActionGoal            
             new_target_pub.publish(MoveBase_msg)
 #while robot is moving towards the random target
             print('\nRobot is moving towards the target position.')
             sleep(15)
             target_reached_status = 0
-#after reaching the target
+#After reaching the traget
             while(target_reached_status == 0):
                 sleep(1)
             print('\nRobot reached the target position.')
- #when the user enters 2 as input the user interface will ask to pick a target out of given targets           
+#When the user enters 2 as input the user interface will ask to pick a traget out of goven tragets            
         elif (x == 2):
             resp = wall_follower_client(False)
 
@@ -96,33 +99,32 @@ def main():
 
         
             MoveBase_msg = MoveBaseActionGoal()
-#setting frame_id to the map            
+#Setting frame_id to the map            
             MoveBase_msg.goal.target_pose.header.frame_id = "map"
-#set orientation W           
+#Set orientation W            
             MoveBase_msg.goal.target_pose.pose.orientation.w = 1
-#for using x input for coordinate           
+#For using x input for coordinate
             MoveBase_msg.goal.target_pose.pose.position.x = random_targets[user_input-1][0]
-#for using y input for coordinate            
+#For using y input for coordinate            
             MoveBase_msg.goal.target_pose.pose.position.y = random_targets[user_input-1][1]
-#publishing MoveBase_msg in type of MoveBaseActionGoal           
+#Publishing MoveBase_msg in type of MoveBaseActionGoal            
             new_target_pub.publish(MoveBase_msg)
-            
-
+#While robot moving towards the target
             print('\nRobot is moving towards the target position.')
             sleep(15)
             target_reached_status = 0
-
+#After reaching the target
             while(target_reached_status == 0):
                 sleep(1)
                 print('\nRobot has reached the target position.')
 
-#when the user gives input as 3 the robot starts following the walls which are present in the map        
+#when the user gives input as 3 the robot starts follwing the walls which are present in the map        
         elif (x == 3):
             resp = wall_follower_client(True)
             print('\nRobot is demonstrating wall-following behavior.')
-#when input is equal to 4 the velocities are set to zero the robot goes to standby mode            
+#when input is equal to 4, the velocities are set to zero the robot goes to standby mode     
         elif (x == 4):
-#function used to stop the robot by passing 0 velocity on the topic Twist        
+#Function used to stop the robot by passing 0 velocity on the topic twist        
             resp = wall_follower_client(False)
             twist_msg = Twist()
             twist_msg.linear.x = 0
